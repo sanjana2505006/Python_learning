479055edb6cf8d764b8d30038b1776064557a6c3
= <=>
2024-11-20 15:19:58 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+# print("     /|")
+# print("    / |")
+# print("   /  |")
+# print("  /   |" )
+# print(" /    |")
+# print("/_____|")
+
+
+name = "Sanjana"
+age = 18
+print(f"My name is {name} and I am {age} years old.") #method3
+character_name = "Annie"
+character_age = 18
+print("hi,my name is",character_name,sep=" ")  # method 1
+print("hi,my name is "+ character_name) # method 2
+
+
+
